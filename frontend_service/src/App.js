6e35ee9d3130f98ca89614c30ac1e62b8436c018
dvoyakiy <dvoyakiyyy@gmail.com>
2021-01-20 22:55:37 +0200

    Add routing, show navbar and homepage

--- frontend_service/src/App.js
+++ frontend_service/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './App.sass';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Navbar } from './components';
+import { Homepage } from './pages';
+
 function App() {
-  return <div className="app" />;
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Homepage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
