c99bd500b7686d0d50ad865adeb8a31df03c14ac
dvoyakiy <dvoyakiyyy@gmail.com>
2021-01-20 22:54:46 +0200

    Create homepage page component

---
+++ frontend_service/src/pages/Homepage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Homepage(props) {
+  return <div>Homepage</div>;
+}
+
+Homepage.propTypes = {};
+
+export default Homepage;
