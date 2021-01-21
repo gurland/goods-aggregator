9e697206092c33dfd5990a47b46ce1e8ffa9fad9
dvoyakiy <dvoyakiyyy@gmail.com>
2021-01-21 20:02:48 +0200

    Move mock data to constants file

--- frontend_service/src/pages/Homepage/index.js
+++ frontend_service/src/pages/Homepage/index.js
@@ -2,89 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './style.scss';
+import { retailChainsMock } from '../../utils/constants';
 
 import { BrandCard } from '../../components';
-
-const retailChains = [
-  {
-    name: 'Novus',
-    avgPricePerKg: 25.44,
-    stores: [
-      {
-        name: 'SkyMall',
-        city: 'kiev',
-        coords: '50.4931815, 30.5581461',
-        lowestPricePerKg: 23,
-      },
-      {
-        name: 'Retroville',
-        city: 'kiev',
-        coords: '50.5039512, 30.4164598',
-        lowestPricePerKg: 21,
-      },
-      {
-        name: 'Николаев',
-        city: 'mykolaiv',
-        coords: '46.9659448, 32.0773213',
-        lowestPricePerKg: 28,
-      },
-    ],
-  },
-  {
-    name: 'Metro',
-    avgPricePerKg: 40,
-    stores: [
-      {
-        name: 'Теремки',
-        city: 'kiev',
-        coords: '50.37886, 30.44221',
-        lowestPricePerKg: 38,
-      },
-      {
-        name: 'Лололошки',
-        city: 'kiev',
-        coords: '50.17886, 30.44221',
-        lowestPricePerKg: 44,
-      },
-      {
-        name: 'Сучі кошки',
-        city: 'kiev',
-        coords: '50.27886, 30.44221',
-        lowestPricePerKg: 42,
-      },
-    ],
-  },
-  {
-    name: 'Auchan',
-    avgPricePerKg: 23.44,
-    stores: [
-      {
-        name: 'Петровка',
-        city: 'kiev',
-        coords: '50.4931815, 30.5581461',
-        lowestPricePerKg: 23,
-      },
-      {
-        name: 'Головка',
-        city: 'lviv',
-        coords: '50.4931815, 30.5581461',
-        lowestPricePerKg: 20,
-      },
-      {
-        name: 'Скуковка',
-        city: 'kharkiv',
-        coords: '50.4931815, 30.5581461',
-        lowestPricePerKg: 21,
-      },
-    ],
-  },
-];
 
 function Homepage(props) {
   return (
     <div className="main-content">
       <div className="cards-wrap">
-        {retailChains.map((brand) => (
+        {retailChainsMock.map((brand) => (
           <BrandCard brandData={brand} key={brand.name} />
         ))}
       </div>
