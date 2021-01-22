59456ac7dab23e0749512d24a83eeb4d9b5d00a6
dvoyakiy <dvoyakiyyy@gmail.com>
2021-01-22 22:25:53 +0200

    Create details page basic layout

--- frontend_service/src/pages/Details/index.js
+++ frontend_service/src/pages/Details/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
-import { ProductTable } from '../../components';
+import { Grid } from 'semantic-ui-react';
+import { ProductTable, PriceGraph, DetailsPageFilters } from '../../components';
+
+import './style.scss';
 
 function Details() {
   return (
     <div className="details-page">
-      <div className="details-page__wrapper">
-        <div className="graph" /> {/* <-- Рома, в'єби тут графік */}
-        <ProductTable />
-      </div>
+      <Grid centered className="details-page__grid">
+        <Grid.Row columns={2}>
+          <Grid.Column width={13}>
+            <PriceGraph className="details-page__price-graph" />
+            <ProductTable className="details-page__product-table" />
+          </Grid.Column>
+          <Grid.Column width={3}>
+            <DetailsPageFilters className="details-page__filters" />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 }
