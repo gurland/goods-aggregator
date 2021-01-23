8e11b5face90d819832c94b2a30dc9c6c62ff233
dvoyakiy <dvoyakiyyy@gmail.com>
2021-01-23 20:41:42 +0200

    Add columns to table

--- frontend_service/src/components/ProductsTable/constants.js
+++ frontend_service/src/components/ProductsTable/constants.js
@@ -1,8 +1,21 @@
 export const productsColumns = [
   {
+    header: '',
+    selector: 'producer',
+    cell: (original) => {
+      const logoLink = original.producer?.logo?.s64x64;
+      return logoLink ? <img src={original.producer?.logo?.s64x64} alt={original.producer?.name} /> : null;
+    },
+  },
+  {
     header: 'Назва товару',
     selector: 'title',
     cell: (original) => original.title,
+  },
+  {
+    header: 'Description',
+    selector: 'description',
+    cell: (original) => original.description || '-',
   },
   {
     header: 'Вага',
@@ -13,5 +26,10 @@ export const productsColumns = [
     header: 'Ціна',
     selector: 'price',
     cell: (original) => original.price,
+  },
+  {
+    header: 'URL',
+    selector: 'web_url',
+    cell: (original) => <a href={original.web_url}>Open page</a>,
   },
 ];
