ae117d15b23100cb206032db87749258e843592c
dvoyakiy <dvoyakiyyy@gmail.com>
2021-01-24 14:59:23 +0200

    Add 'go back' button on details page

--- frontend_service/src/components/Navbar/index.js
+++ frontend_service/src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 
 import { Menu, Input, Radio, Icon } from 'semantic-ui-react';
 import { links } from '../../utils/constants';
@@ -11,21 +11,25 @@ import { MapModal } from '../index';
 
 function Navbar(props) {
   const { dispatch, state } = useContext(store);
+  const history = useHistory();
 
-  const showBurger = useLocation().pathname === links.details;
+  const showIcons = useLocation().pathname === links.details;
   const toggleSidebar = () => dispatch({ type: actions.SIDEBAR_TOGGLE, payload: !state.sidebarVisible });
+  const goToHomepage = () => history.push(links.homepage);
 
   return (
     <span className="navbar">
       {props.position === 'top' && (
         <Menu fluid widths={3}>
-          <Menu.Item />
+          <Menu.Item>
+            {showIcons && <Icon name="arrow left" className="navbar-icon arrow-icon" onClick={goToHomepage} />}
+          </Menu.Item>
           <Menu.Item>
             <Input className="icon" iconPosition="left" icon="search" placeholder="Search..." />
           </Menu.Item>
           <Menu.Item position="right">
             <Radio toggle className="theme-toggle" />
-            {showBurger && <Icon name="bars" className="burger-menu" onClick={toggleSidebar} />}
+            {showIcons && <Icon name="bars" className="navbar-icon burger-menu" onClick={toggleSidebar} />}
           </Menu.Item>
         </Menu>
       )}
