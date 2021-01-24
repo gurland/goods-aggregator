331827189394d688515b26f7082c1c2800c28cc7
dvoyakiy <dvoyakiyyy@gmail.com>
2021-01-24 17:52:17 +0200

    Make dark theme radio button controlled

--- frontend_service/src/components/Settings/index.js
+++ frontend_service/src/components/Settings/index.js
@@ -21,6 +21,7 @@ const languages = [
 function Settings() {
   const { state, dispatch } = useContext(store);
   const onLanguageSelect = (event, { value }) => dispatch({ type: actions.SET_CONTENT_LANGUAGE, payload: value });
+  const onDarkThemeToggle = () => dispatch({ type: actions.DARK_THEME_TOGGLE, payload: !state.darkTheme });
 
   return (
     <Grid>
@@ -28,7 +29,7 @@ function Settings() {
         <Header>Settings</Header>
         <Grid.Row className="settings-row__first">
           <Header sub>Dark theme</Header>
-          <Radio toggle className="theme-toggle" />
+          <Radio toggle className="theme-toggle" onClick={onDarkThemeToggle} checked={state.darkTheme} />
         </Grid.Row>
         <Grid.Row>
           <Header sub>Content language</Header>
